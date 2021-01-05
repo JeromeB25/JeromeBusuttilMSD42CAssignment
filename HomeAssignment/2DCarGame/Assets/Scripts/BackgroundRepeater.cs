6e50ba6606ee83c4e388ea517a3c4ef42435c907
JeromeB25 <73173416+JeromeB25@users.noreply.github.com>
2021-01-05 17:03:44 +0100

    Task 1 (1)
    Finished some questions in Task1

---
+++ HomeAssignment/2DCarGame/Assets/Scripts/BackgroundRepeater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRepeater : MonoBehaviour
+{
+    [SerializeField] float backgroundRepeaterSpeed = 0.15f;
+
+    //the material from the texture
+    Material myMaterial;
+
+    //movement offset
+    Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get the material of Background from Renderer component
+        myMaterial = GetComponent<Renderer>().material;
+
+        //move in the y-axis at the given speed
+        offSet = new Vector2(0f, backgroundRepeaterSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the texture of the material by offSet every frame
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
---
+++ HomeAssignment/2DCarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float playerMoveSpeed = 10f;
+    [SerializeField] float padding = 0.1f; 
+
+    float xMin, xMax, yMin, yMax; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameCamera();
+    }
+
+    private void GameCamera()
+    {
+        Camera gamecamera = Camera.main;
+
+        xMin = gamecamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gamecamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gamecamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gamecamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding; 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        playerMove();   
+    }
+
+    private void playerMove()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * playerMoveSpeed;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+
+        transform.position = new Vector2(newXPos, yMin); 
+    }
+}
